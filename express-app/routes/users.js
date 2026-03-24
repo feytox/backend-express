b28ac84e18c16e4c3a39925d9cd9790010235c42
Feytox <73577753+feytox@users.noreply.github.com>
2026-03-24 17:10:26 +0500

    feat: add sqlite3 (8.1)

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("mydb.db");
+
+db.run(`CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name text
+)`);
+
 const users = [
     { id: 1, name: "Булатов Александр" },
     { id: 2, name: "Рахимзянов Равиль" },
