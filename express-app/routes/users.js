36ffc336e29c7d00ce8861cab5d9c97677228f57
Feytox <73577753+feytox@users.noreply.github.com>
2026-03-24 16:57:43 +0500

    feat: add users/ (3)

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const users = [
+    { id: 1, name: "Булатов Александр" },
+    { id: 2, name: "Рахимзянов Равиль" },
+];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+router.get("/", function (req, res, next) {
+    res.json({
+        items: users,
+    });
 });
 
 module.exports = router;
